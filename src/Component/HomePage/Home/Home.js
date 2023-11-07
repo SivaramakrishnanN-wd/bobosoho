d553276251cc384a7da747c0ef9bb74fef9f3170
SivaramakrishnanN-wd <shivamaya192@gmail.com>
2023-11-07 15:20:46 +0530

    change

--- src/Component/HomePage/Home/Home.js
+++ src/Component/HomePage/Home/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
 // import BgVideo from "../../../assets/video.mp4"
 import BannerSection from "../BannerSection/BannerSection";
-import Carousel from "../Carousel/Carousel"
+import Carousel from "../Carousel/Carousel";
 import Cloud from "../Carousel/Cloud/Cloud";
 import Cyber from "../Cyber/Cyber";
-import "./Home.css"
+import "./Home.css";
 
 export default function Home() {
   return (
     <div>
-     <BannerSection /> {""} 
-      <Carousel />
+      <BannerSection />
       <Cloud />
+
+      <Carousel />
       <Cyber />
     </div>
   );
